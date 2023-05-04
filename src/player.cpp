c81855aadb1215313db23b7a54f8451f40fd0f34
kirkto7 <mca.kirkton@gmail.com>
2023-05-03 22:23:12 -0500

    small changes

--- src/player.cpp
+++ src/player.cpp
@@ -1,5 +1,7 @@
 #include "player.h"
+#include <iostream>
 
+using namespace std;
 
 bool Player::move(char direction) {
     int nextx = currx;
@@ -20,8 +22,67 @@ bool Player::move(char direction) {
       default:
         break;
     }
+    if(nextx > map.WIDTH || nextx < 0 || nexty > map.HEIGHT || nexty < 0) {
+      return false;
+    }
+    MapCell* cell = map.getCell(nextx, nexty);
+    map.getCell(currx, curry)->vacate();
+    if(cell->hasCE()) {
+        cout << "You run into a CE, trapping you in a conversation about assembly";
+        isDead = true;
+        return false;
+    }
+    if(cell->hasEntity()) {
+        //cell.entity->do();
+        return false;
+    }
+    cell->enter();
+    currx = nextx;
+    curry = nexty;
 }
 
-int Player::shoot(char direction) {
-    ammo -= 1;
+
+bool Player::shoot(char direction) {
+    if(ammo > 0) {
+        ammo -= 1;
+        switch(tolower(direction)) {
+            case 'e':
+                cleanCE(currx, map.WIDTH, true);
+                break;
+            case 'w':
+                cleanCE(currx, 0, true);
+                break;
+            case 's':
+                cleanCE(curry, map.HEIGHT, false);
+                break;
+            case 'n':
+                cleanCE(curry, 0, false);
+                break;
+            default:
+                break;
+        }
+    } else {
+        return false;
+    }
 }
+
+void Player::cleanCE(int val, int max, bool isX) {
+    int dir = max > val ? 1 : -1; //sets direction of the deodorant 
+    val+=dir;
+    while(val != max) { 
+        MapCell* cell = isX ? map.getCell(val, curry) : map.getCell(currx, val);
+        if(cell->hasCE()) {
+            //remove CE
+            cout << "You hit the CE with deodorant!";
+            break;
+        }
+        val += dir;
+    }
+}
+
+void Player::place(int x, int y) {
+    currx = x;
+    curry = y;
+    move('x');
+}
+
--- src/player.h
+++ src/player.h
@@ -1,18 +1,21 @@
 #include "map.h"
+#include "cell.h"
 #include <string>
 
 class Player {
 
     public:
-        Player(Map *map, int startx, int starty) : map(*map), currx(startx), curry(starty), ammo(5){}
+        Player(Map *map, int startx, int starty) : map(*map), currx(startx), curry(starty), ammo(5), isDead(false){}
 
         bool move(char direction);
-        int shoot(char direction);
+        bool shoot(char direction);
+        void place(int x, int y);
 
     private:
-        Map* map;
+        void cleanCE(int val, int max, bool isX);
+        Map map;
         int currx;
         int curry;
         int ammo;
-
-}
+        bool isDead;
+};
